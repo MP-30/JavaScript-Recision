aafa0f383a73eae44d42feaa8dffa3debccfcf80
Aditya Bhadauriya <adityabhadauriya93@gmail.com>
2023-12-30 16:30:33 +0530

    Add files via upload
    06

---
+++ 01_Basics/06_numsAndMaths.js
@@ -0,0 +1,31 @@
+const score = 400
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(2))
+
+const otherNumber = 123.91566
+//console.log(otherNumber.toFixed(2))
+//console.log(otherNumber.toPrecision(3))
+
+const humdereds = 1000000
+//console.log(humdereds.toLocaleString('en-IN'))
+
+
+//*********************Maths******** */
+
+console.log(Math.abs(-5656))
+console.log(Math.round(4.8))
+console.log(Math.ceil(4.1))
+console.log(Math.floor(4.9))
+console.log(Math.min(4,8,8,6,3))
+console.log(Math.max(4,8,8,6,3))
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10)+1)
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max -min + 1)) + min)
