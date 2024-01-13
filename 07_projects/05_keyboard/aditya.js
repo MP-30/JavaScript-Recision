0b2e36ccd19d4f0f0e7558b66909b83ca673139b
MP-30 <adityabhadauriya93@gmail.com>
2024-01-13 22:55:17 +0530

    keyboard_key_project

---
+++ 07_projects/05_keyboard/aditya.js
@@ -0,0 +1,25 @@
+const insert = document.querySelector('#insert')
+
+window.addEventListener('keydown', (e)=>{
+  insert.innerHTML= `
+    <div class='color'>
+    </div>
+    <table>
+  <tr>
+    <th>key</th>
+    <th>keycode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>
+  `
+})
