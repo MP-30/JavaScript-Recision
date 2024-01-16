c14e63284e31775d0ce81209608dac8f9119a175
MP-30 <adityabhadauriya93@gmail.com>
2024-01-16 13:35:21 +0530

    objects

--- 10_class_oops/oop.js
+++ 10_class_oops/oop.js
@@ -1,7 +1,31 @@
 const user = {
   username: "Aditya",
   loginCount: 6,
-  signed: true
+  signed: true,
+  getUserDetails: function(){
+    //console.log("Got user details from databases");
+    //console.log(`Username:${this.username}`);
+    //console.log(this);
+  }
 }
 
-console.log();
+//console.log(user.loginCount);
+//console.log(user.getUserDetails());
+//console.log(this)
+
+function User(username, loginCount, isLoggedIn){
+  this.username = username;
+  this.loginCount = loginCount;
+  this.isLoggedIn = isLoggedIn;
+
+  this.greeting = function(){
+  //  console.log(`Welcome ${this.username}`)
+  }
+
+  return this
+}
+
+const userOne = new User("aditya", 45, true)
+const userTwo = new User("Itsme", 89, true)
+console.log(userOne.constructor);
+//console.log(userTwo);
